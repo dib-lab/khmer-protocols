7438a1f5d912ca11486097f4bec19858dda3f889
Leigh Sheneman <leighs@msu.edu>
2014-02-24 19:00:37 -0500

    Update documents

--- mrnaseq/scan.sh
+++ mrnaseq/scan.sh
@@ -2,29 +2,24 @@
 
 #set -x
 
-script=$1.sh
-<<<<<<< HEAD
-echo > ${script}
+script=${1}.sh
+echo ${script}
 
 #clearing out the IFS to make bash not strip out leading whitespace
 IFS=''
 inshell=FALSE #wrapper
 incode=FALSE  #exe code-block flag
 looped=FALSE  #switch for end of exe code-block
-=======
-rm = $1.sh
+rm ${script}
 
 #clearing out the IFS to make bash not strip out leading whitespace
 IFS=''
 inshell=FALSE
->>>>>>> 3dddaf7e3269d2a9bc9ae704621192f0e6d5b993
 
 while read line
 do
 	if [ "$line" = ".. shell:: start" ]
 	then
-<<<<<<< HEAD
-		
 		inshell=TRUE	
 	elif [ "$line" = ".. shell:: stop" ]
 	then
@@ -47,17 +42,3 @@ do
 done < $1
 
 #source ${script}
-=======
-		inshell=TRUE	
-	elif [ "$line" = ".. shell:: stop" ]
-	then
-		inshell=FALSE
-	fi
-	if [ "$inshell" = "TRUE" ] && ( echo $line | grep '^   ' ) 
-	then
-		echo $line >> ${script}
-	fi
-done < $1
-
-#source ${script}
->>>>>>> 3dddaf7e3269d2a9bc9ae704621192f0e6d5b993
